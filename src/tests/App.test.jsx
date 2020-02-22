1406196b19f6d99d427182f0a37375442ff24c93
Jeffrey Penkar <=>
2020-02-22 13:48:29 -0600

    Data-testid replaced in tests / UI.

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -2,27 +2,29 @@ import setUpApplication from './shared';
 
 test('Renders the "Not the WaPo" application', () => {
   const { getByText } = setUpApplication({ url: '' });
-  const linkElement = getByText(/Not the Washington Post/i);
 
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText(/Not the Washington Post/)).toBeInTheDocument();
 });
 
 test('Should contain the nine teasers articles, and no main articles.', () => {
-  const { container } = setUpApplication({ url: '' });
-  const teasers = container.querySelectorAll('[data-iden="teaser-article"]');
-  const mainArticle = container.querySelectorAll('[data-iden="main-article"]');
+  const { getAllByTestId } = setUpApplication({ url: '' });
+  const teasers = getAllByTestId('teaser-article');
 
   expect(teasers).toHaveLength(9);
-  expect(mainArticle).toHaveLength(0);
   teasers.forEach((element) => expect(element).toBeInTheDocument());
 });
 
 test('Should contain one main article and no teasers.', () => {
-  const { container } = setUpApplication({ url: '/article/0002' });
-  const teasers = container.querySelectorAll('[data-iden="teaser-article"]');
-  const mainArticle = container.querySelectorAll('[data-iden="main-article"]');
+  const { getAllByTestId } = setUpApplication({ url: '/article/0002' });
+  const mainArticle = getAllByTestId('main-article');
 
-  expect(teasers).toHaveLength(0);
   expect(mainArticle).toHaveLength(1);
+});
+
+test('Should contain four teaser articles when looking through a section.', () => {
+  const { getAllByTestId } = setUpApplication({ url: '/section/politics' });
+  const teasers = getAllByTestId('teaser-article');
+
+  expect(teasers).toHaveLength(4);
   teasers.forEach((element) => expect(element).toBeInTheDocument());
 });
--- src/tests/shared.jsx
+++ src/tests/shared.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
